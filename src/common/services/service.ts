d2b60640dc08b5323d0e03ed9b3dda0c8d4b4c39
Khai Nguyen <khai.fordev@gmail.com>
2023-11-04 23:19:32 +0700

    clear axios service

--- src/common/services/service.ts
+++ src/common/services/service.ts
@@ -1,7 +1,3 @@
-import { IBase, ILoginResponse, IResponseBody } from "../interfaces/base";
-import { IUser, IUserLogin } from "../interfaces/user";
-import instance from "./config";
-
 const HOST_SERVER = "http://localhost:3200";
 
 export const ENDPOINTS = {
@@ -11,27 +7,5 @@ export const ENDPOINTS = {
 }
 
 export const API_SERVICES = {
-    register: async (user: IUser) => {
-        try {
-            await instance.post(ENDPOINTS.register, user);
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    login: async (user: IUserLogin): Promise<IResponseBody<ILoginResponse> | undefined> => {
-        try {
-            const { data } = await instance.post<IResponseBody<ILoginResponse>>(ENDPOINTS.login, user);
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    me: async (): Promise<IResponseBody<IUser & IBase> | undefined> => {
-        try {
-            const { data } = await instance.get<IResponseBody<IUser & IBase>>(ENDPOINTS.me);
-            return data;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+
 }
