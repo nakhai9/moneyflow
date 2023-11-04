570f478f6230ad3acd69b1dacbd4ebe134aa1043
Khai Nguyen <khai.fordev@gmail.com>
2023-11-04 23:01:05 +0700

    feat: add wallet to clould firestore

--- src/common/interfaces/base.ts
+++ src/common/interfaces/base.ts
@@ -1,3 +1,8 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface IBase {
-    
+    id?: string;
+    createdAt: Timestamp;
+    updatedAt: Timestamp | null;
+    isDelete: boolean;
 }
