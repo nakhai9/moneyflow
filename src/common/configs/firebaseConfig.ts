519d31135e512a4db1893238708117f12994ae39
Khai Nguyen <khai.fordev@gmail.com>
2023-11-07 13:40:36 +0700

    feat: implement .env file

--- src/common/configs/firebaseConfig.ts
+++ src/common/configs/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyAOXIJxreOiB2fb6rhFZWs74D3y3GsPuY8",
-    authDomain: "firestore-project-6ae19.firebaseapp.com",
-    projectId: "firestore-project-6ae19",
-    storageBucket: "firestore-project-6ae19.appspot.com",
-    messagingSenderId: "516848345418",
-    appId: "1:516848345418:web:759d0f0de11bf569df61b3",
-    measurementId: "G-8SMHCXW33C"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 export const app = initializeApp(firebaseConfig);
